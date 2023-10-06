ff6291441a09aa7a37a468736cc57ba74064d9ba
thguss <thgus345@naver.com>
2023-10-06 23:00:08 +0900

    [Section4] Users Microservice - Spring Security 연동

--- user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
+++ user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
@@ -4,6 +4,8 @@ import java.util.UUID;
 
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.example.userservice.dto.UserDto;
@@ -17,6 +19,7 @@ import lombok.RequiredArgsConstructor;
 public class UserServiceImpl implements UserService {
 
 	private final UserRepository userRepository;
+	private final BCryptPasswordEncoder passwordEncoder;
 
 	@Override
 	public UserDto createUser(UserDto userDto) {
@@ -25,7 +28,7 @@ public class UserServiceImpl implements UserService {
 		ModelMapper mapper = new ModelMapper();
 		mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
 		UserEntity userEntity = mapper.map(userDto, UserEntity.class);
-		userEntity.setEncryptedPwd("encrypted_password");
+		userEntity.setEncryptedPwd(passwordEncoder.encode(userDto.getPwd()));
 
 		userRepository.save(userEntity);
 
