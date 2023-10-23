c125e3ef844edac9a0d25668a7f13c37a0c1fa5b
thguss <thgus345@naver.com>
2023-10-23 17:22:10 +0900

    [Section6] AuthenticationFilter 추가

--- user-service/src/main/java/com/example/userservice/security/WebSecurity.java
+++ user-service/src/main/java/com/example/userservice/security/WebSecurity.java
@@ -1,28 +1,61 @@
 package com.example.userservice.security;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.ObjectPostProcessor;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.IpAddressMatcher;
+
+import com.example.userservice.service.UserService;
+
+import lombok.RequiredArgsConstructor;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class WebSecurity {
+	private final UserService userService;
+	private final BCryptPasswordEncoder bCryptPasswordEncoder;
+	private final ObjectPostProcessor<Object> objectPostProcessor;
+
+	private static final String[] WHITE_LIST = {
+		"/users/**",
+		"/",
+		"/**"
+	};
+
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		return http
 			.csrf(CsrfConfigurer::disable)
 			.authorizeHttpRequests(authorizeRequests ->
 				authorizeRequests
-					.requestMatchers(new AntPathRequestMatcher("/h2-console/**")).permitAll()
-					.requestMatchers(new AntPathRequestMatcher("/user-service/**")).permitAll()
-					.requestMatchers(new AntPathRequestMatcher("/error")).permitAll()
+					// .requestMatchers(WHITE_LIST).permitAll()
+					.requestMatchers(PathRequest.toH2Console()).permitAll()
+					.requestMatchers(new IpAddressMatcher("127.0.0.1")).permitAll()
 			)
+			.addFilter(getAuthenticationFilter())
 			.headers(header -> header.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
 			.build();
+	}
+
+	public AuthenticationManager authenticationManager(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userService).passwordEncoder(bCryptPasswordEncoder);
+		return auth.build();
+	}
+
+	private AuthenticationFilter getAuthenticationFilter() throws Exception {
+		AuthenticationFilter authenticationFilter = new AuthenticationFilter();
+		AuthenticationManagerBuilder builder = new AuthenticationManagerBuilder(objectPostProcessor);
+		authenticationFilter.setAuthenticationManager(authenticationManager(builder));
+		return authenticationFilter;
 	}
 }
