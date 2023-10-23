398422d2e8ebfd52296d276b0682559952e232f0
thguss <thgus345@naver.com>
2023-10-23 23:13:12 +0900

    [Section6] 로그인 처리 과정

--- user-service/src/main/java/com/example/userservice/security/AuthenticationFilter.java
+++ user-service/src/main/java/com/example/userservice/security/AuthenticationFilter.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import com.example.userservice.vo.RequestLogin;
@@ -15,7 +16,9 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 	@Override
 	public Authentication attemptAuthentication(
@@ -44,6 +47,6 @@ public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 		FilterChain chain,
 		Authentication authResult
 	) throws IOException, ServletException {
-		// 로그인 성공할 때
+		log.debug(((User)authResult.getPrincipal()).getUsername());
 	}
 }
