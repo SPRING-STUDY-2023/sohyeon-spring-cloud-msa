e35cb46812f0dfd2ed9c703b30a3cd899e51ef23
thguss <thgus345@naver.com>
2023-11-20 16:34:53 +0900

    [Section10] RestTemplate 사용

--- user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
+++ user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
@@ -2,15 +2,21 @@ package com.example.userservice.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.UUID;
 
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.example.userservice.dto.UserDto;
 import com.example.userservice.jpa.UserEntity;
@@ -22,9 +28,10 @@ import lombok.RequiredArgsConstructor;
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
-
 	private final UserRepository userRepository;
 	private final BCryptPasswordEncoder passwordEncoder;
+	private final RestTemplate restTemplate;
+	private final Environment env;
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -63,10 +70,26 @@ public class UserServiceImpl implements UserService {
 
 		UserDto userDto = new ModelMapper().map(userEntity, UserDto.class);
 
-		List<ResponseOrder> orders = new ArrayList<>();
+		// List<ResponseOrder> orders = new ArrayList<>();
+
+		/* Using as rest template */
+		List<ResponseOrder> orders = getOrdersByRestTemplate(userId);
+
 		userDto.setOrders(orders);
 
 		return userDto;
+	}
+
+	private List<ResponseOrder> getOrdersByRestTemplate(String userId) {
+		String orderUrl = String.format(Objects.requireNonNull(env.getProperty("order_service.url")), userId);
+		ResponseEntity<List<ResponseOrder>> ordersResponse = restTemplate.exchange(
+			orderUrl,
+			HttpMethod.GET,
+			null,
+			new ParameterizedTypeReference<List<ResponseOrder>>() {
+			}
+		);
+		return ordersResponse.getBody();
 	}
 
 	@Override
